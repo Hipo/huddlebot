eb51ad243eef58d84fc8e64328af0a1d5d9e7f44
Taylan Pince <taylanpince@gmail.com>
2019-09-26 15:24:38 +0300

    Implement basic Slack command infrastructure

--- huddlebot/slack/urls.py
+++ huddlebot/slack/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from huddlebot.slack.views import SlackOAuthView
+from huddlebot.slack.views import SlackOAuthView, SlackCommandView
 
 
 urlpatterns = [
     path(r'oauth/', SlackOAuthView.as_view(), name='slack-oauth'),
+    path(r'command/', SlackCommandView.as_view(), name='slack-command'),
 ]
--- huddlebot/slack/views.py
+++ huddlebot/slack/views.py
@@ -1,8 +1,10 @@
 from django.conf import settings
 from django.core.exceptions import SuspiciousOperation, PermissionDenied
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
 from django.shortcuts import render
+from django.utils.decorators import method_decorator
 from django.views import View
+from django.views.decorators.csrf import csrf_exempt
 
 from huddlebot.slack.models import SlackWorkspace, SlackChannel
 
@@ -11,7 +13,21 @@ import slack
 
 
 SLACK_OAUTH_URL = "https://slack.com/api/oauth.access"
-SLACK_OAUTH_SCOPES = "channels:read,chat:write:bot"
+SLACK_OAUTH_SCOPES = "channels:read,chat:write:bot,commands"
+
+SLACK_COMMAND_AUTHENTICATE = 'authenticate'
+SLACK_COMMAND_ADD_EVENT = 'add_event'
+SLACK_COMMAND_REMOVE_EVENT = 'remove_event'
+SLACK_COMMAND_UPDATE_CHANNELS = 'update_channels'
+SLACK_COMMAND_SHOW_EVENTS = 'show_events'
+
+ALLOWED_COMMANDS = [
+    SLACK_COMMAND_SHOW_EVENTS,
+    SLACK_COMMAND_UPDATE_CHANNELS,
+    SLACK_COMMAND_REMOVE_EVENT,
+    SLACK_COMMAND_ADD_EVENT,
+    SLACK_COMMAND_AUTHENTICATE,
+]
 
 
 class SlackOAuthView(View):
@@ -54,3 +70,48 @@ class SlackOAuthView(View):
         workspace.update_channels()
         
         return render(request, 'slack/success.html')
+
+
+@method_decorator(csrf_exempt, name='dispatch')
+class SlackCommandView(View):
+    """
+    Handles incoming commands from Slack
+    """
+    def post(self, request):
+        command = request.POST.get('text').strip()
+        team_id = request.POST.get('team_id')
+        user_id = request.POST.get('user_id')
+        channel_id = request.POST.get('channel_id')
+        
+        if command not in ALLOWED_COMMANDS:
+            return JsonResponse({
+                'response_type': 'ephemeral',
+                'text': "Sorry, I don't know this command. Please try one of the allowed commands."
+            })
+
+        try:
+            workspace = SlackWorkspace.objects.get(team_id=team_id)
+        except:
+            return JsonResponse({
+                'response_type': 'ephemeral',
+                'text': "Your workspace doesn't seem to be setup, please install Huddlebot first."
+            })
+
+        message = ''
+
+        if command == SLACK_COMMAND_AUTHENTICATE:
+            message = 'Follow this link to authenticate your Google Calendar account: '
+        elif command == SLACK_COMMAND_ADD_EVENT:
+            pass
+        elif command == SLACK_COMMAND_SHOW_EVENTS:
+            pass
+        elif command == SLACK_COMMAND_SHOW_EVENTS:
+            pass
+        elif command == SLACK_COMMAND_UPDATE_CHANNELS:
+            workspace.update_channels()
+            
+            message = 'OK, all done! I have updated your channels.'
+        
+        return JsonResponse({
+            'text': message,
+        })
