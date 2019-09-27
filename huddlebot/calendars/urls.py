17242ddc4e1c64b482f779e75fe727af5f3995ad
Efe Öge <efeoge@gmail.com>
2019-09-27 17:19:51 +0300

    Have calendars prefix in base urls.py

--- huddlebot/calendars/urls.py
+++ huddlebot/calendars/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from huddlebot.calendars.views import CalendarAuthCallbackView
 
 urlpatterns = [
-    path(r'calendars/auth-callback/', CalendarAuthCallbackView.as_view(), name='auth-callback'),
+    path(r'auth-callback/', CalendarAuthCallbackView.as_view(), name='auth-callback'),
 ]
--- huddlebot/urls.py
+++ huddlebot/urls.py
@@ -7,6 +7,6 @@ from django.urls import include
 urlpatterns = [
     path('admin/', admin.site.urls),
     url(r'^slack/', include(('huddlebot.slack.urls', 'slack'), namespace='slack')),
+    url(r'^calendars/', include(('huddlebot.calendars.urls', 'calendars'), namespace='calendars')),
     url(r'^', include('hipo_django_core.urls')),
-    url(r'^', include(('huddlebot.calendars.urls', 'calendars'), namespace='calendars')),
 ]
